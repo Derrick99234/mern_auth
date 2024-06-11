88994d68f19c514d786880fea3a098c21a3412ec
derrick99234 <pshubomi@gmail.com>
2024-06-11 21:38:40 +0100

    createed user model

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connect to mongoose database");
+  })
+  .catch(() => {
+    console.log("Error occur while tryying to  connect to mongoose");
+  });
 const app = express();
 
 app.listen(3000, () => console.log("Server running on port 3000"));
