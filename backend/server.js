992d7aa89ad591e19645053bc8ec12e4192e918c
derrick99234 <pshubomi@gmail.com>
2024-06-11 23:53:26 +0100

    completed the sign up UI

--- backend/server.js
+++ backend/server.js
@@ -21,3 +21,14 @@ app.listen(3000, () => console.log("Server running on port 3000"));
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+
+  return res.status(statusCode).json({
+    error: false,
+    message,
+    statusCode,
+  });
+});
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,45 @@
+import { Link } from "react-router-dom";
+
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7 ">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="bg-slate-100 p-3 outline-none rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="bg-slate-100 p-3 outline-none rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="bg-slate-100 p-3 outline-none rounded-lg"
+        />
+        <button
+          disabled
+          className="bg-slate-700 p-3 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          Sign up
+        </button>
+      </form>
+      <div className="">
+        <p className="flex gap-2 mt-5">
+          Alredy have an acct?{" "}
+          <span className="text-blue-500">
+            <Link to="/login">Sign in</Link>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
